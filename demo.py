c73a2474f88a6fadf2c6a19cb77d16eadf66ae82
pallav sharma <pallavsharma334@gmail.com>
2026-01-30 12:20:26 +0530

    m

--- demo.py
+++ demo.py
@@ -1,12 +1,9 @@
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
-print("Addition:", a + b)
+print("Addition:", a + c)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
-print("Division:", a / b)
+print("Division:", a / g)
 ?";'..'KeyboardInterrupt
 sdicnsd
-git "
-wuefhidcsmxc
--wqdoals
